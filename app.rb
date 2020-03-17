7487456b2697de45928f0f34d093a72cb05e8a78
Jason Barr <Barr75@gmail.com>
2020-03-17 16:47:25 -0700

    Uses caesar cipher to encrypt a phrase

--- app.rb
+++ app.rb
@@ -2,5 +2,31 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  
+  phrase = params['phrase']
+  encrypted = caesar_cipher(phrase, 1) if phrase
+  erb :index, :locals => {:encrypted => encrypted}
+end
+
+def caesar_cipher(str, n)
+  character_array = str.split("").map {|char| char.ord}
+  character_array.each_with_index do |char, index|
+      if (97..122).include? char
+          if char + n > 122
+              character_array[index] = char+n-26
+          elsif char + n < 97
+              character_array[index] = char+n+26
+          else
+              character_array[index] = char + n
+          end
+      elsif (65..90).include? char
+          if char + n > 90
+              character_array[index] = char+n-26
+          elsif char + n < 65
+              character_array[index] = char+n+26
+          else
+              character_array[index] = char + n
+          end
+      end
+  end
+  character_array.map{|char| char.chr}.join("")
 end
